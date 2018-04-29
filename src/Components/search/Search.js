6f467b15132dd89a0714b8c16a73c438ef7a6cea
AaronTheDesigner <flux.charlotte@gmail.com>
2018-04-29 18:36:05 -0400

    axios added to onTextChange

--- src/Components/search/Search.js
+++ src/Components/search/Search.js
@@ -13,13 +13,16 @@ class Search extends Component {
     images: []
   };
 
-  onTextChange = (e) => {
+  onTextChange = e => {
     this.setState({[e.target.name]: e.target.value}, () => {
-      axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}`)
+      axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}q=${this.state.searchText}&image_type=photo&per_page${this.state.amount}&safesearch=true`)
+      .then(res => this.setState({images: res.data.hits}))
+      .catch(err => console.log(err));
     });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <TextField 
